9ce732e99829cdea523e048a53b4a61e16ad7a7c
DreamerCG <57459672+DreamerCG@users.noreply.github.com>
2026-01-09 15:18:24 +0100

    Update install-BSA.sh

--- install-BSA.sh
+++ install-BSA.sh
@@ -2,22 +2,6 @@
 # BATOCERA - SWITCH ADD-ON
 TERM=xterm clear > /dev/tty 2>/dev/null || printf "\033c" > /dev/tty
 
-
-# Récupération de la version principale de Batocera
-version=$(batocera-es-swissknife --version | grep -oE '^[0-9]+')
-
-# Vérification que la version est bien un nombre
-if [[ -z "$version" ]]; then
-    dialog --msgbox "Impossible de détecter une version valide de Batocera. Installation annulée." 8 60
-    clear
-    exit 1
-fi
-
-echo "Version détectée de Batocera $version"
-exit 1
-
-
-
 set -e
 trap 'rm -f "$temp_file"' EXIT
 
