82f840ea1fa8c1e7a21d40e80090278c3a000524
DreamerCG <57459672+DreamerCG@users.noreply.github.com>
2026-01-25 10:28:32 +0100

    Remove V40

--- install-BSA.sh
+++ install-BSA.sh
@@ -21,7 +21,7 @@ if [[ -z "$version" ]]; then
 fi
 
 # Vérification stricte : uniquement Batocera 43 autorisée
-[[ "$version" =~ ^(40|41|42|43)$ ]] || {
+[[ "$version" =~ ^(41|42|43)$ ]] || {
     echo "ERROR: Batocera non supportée (détectée: $version)"
     echo "Versions supportées : 40, 41, 42, 43"
     exit 1
