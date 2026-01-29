3c169d50e1e25f461eb891f485ecd414dd9ddf50
DreamerCG <57459672+DreamerCG@users.noreply.github.com>
2026-01-29 15:40:18 +0100

    Update dcg_service_update.sh

--- app/dcg_service_update.sh
+++ app/dcg_service_update.sh
@@ -79,8 +79,8 @@ if [ "$toolbox_version_local" != "$toolbox_download_version" ]; then
 
     # echo "[$(date)] DEBUG folder_version=$folder_update_version"
 
-    URL_BASE="https://raw.githubusercontent.com/DreamerCG/BatoceraToolBoxDream/main/install/$folder_update_version/system/switch/configgen/"
-    ULR_BIN="https://raw.githubusercontent.com/DreamerCG/BatoceraToolBoxDream/main/install/$folder_update_version/system/switch/extra/packages/"
+    URL_BASE="https://raw.githubusercontent.com/DreamerCG/BatoceraToolBoxDream/main/install/$folder_update_version/system/switch/configgen"
+    ULR_BIN="https://raw.githubusercontent.com/DreamerCG/BatoceraToolBoxDream/main/install/$folder_update_version/system/switch/extra/packages"
 	URL_ROM_INSTALL="https://raw.githubusercontent.com/DreamerCG/BatoceraToolBoxDream/main/install/roms/switch"
     URL_ROM_IMAGE_INSTALL="https://raw.githubusercontent.com/DreamerCG/BatoceraToolBoxDream/main/install/roms/switch/images"
 
